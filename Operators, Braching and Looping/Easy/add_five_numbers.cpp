3237258fcc40c34f1bb55de945d27988d13da927
Ayush Gupta <ayushgupta.9350@gmail.com>
2021-10-08 10:37:18 +0530

    day 1 done

---
+++ Operators, Braching and Looping/Easy/add_five_numbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1=5;
+int num2=13;
+int num3=7;
+int num4=21;
+int num5=48;
+int sum=0;
+
+sum=num1+num2+num3+num4+num5;
+
+cout << "Sum of five numbers are " << sum;
+return 0;
+}
+
---
+++ Operators, Braching and Looping/Easy/find_min_max_of_two_given_numbers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1=129,num2=251;
+    if(num1>num2){
+        cout << num1 << " is maximum\n"; 
+        cout << num2 << " is mininmum";
+    }
+    else{
+        cout << num1 << " is minimum\n";
+        cout << num2 << " is maximum";
+    }
+    return 0;
+}
